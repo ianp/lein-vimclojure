a76a913e6ef475aaaab00d8ee1411c56969a467c
Ian Phillips <ianp@ianp.org>
2011-12-04 18:27:52 +0100

    Clojure 1.3 support.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.autre/lein-vimclojure "1.0.0"
+(defproject org.clojars.autre/lein-vimclojure "1.1.0"
   :description "A leiningen plugin to launch a vimclojure nailgun server."
   :url "http://github.com/autre/lein-vimclojure"
-  :dependencies [[vimclojure/server "2.2.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]]
-  :repositories { "clojars" "http://clojars.org/repo" })
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [vimclojure/server "2.3.0"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]])
 
